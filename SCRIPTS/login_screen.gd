0dcce9895471b886810c5eeb726d8cbfbf5d73cb
SKY <listangco21@GMAIL.COM>
2024-12-11 21:40:26 +0800

    TRY

--- SCRIPTS/login_screen.gd
+++ SCRIPTS/login_screen.gd
@@ -1,10 +1,8 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,3 +11,6 @@ func _process(delta: float) -> void:
 # goes to signup scene
 func _on_signup_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://SCENES/signup_screen.tscn")
+
+func _on_login_button_pressed() -> void:
+	pass # Replace with function body.

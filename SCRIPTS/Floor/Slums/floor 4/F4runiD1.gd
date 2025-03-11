fb01aefb7e4b082fa7927e1d2a31257c11de1191
SKYRENZ <131676044+SKYRENZ@users.noreply.github.com>
2025-03-11 21:33:33 +0800

    dialogue
    now the player wont be able to move while in dialogue

--- SCRIPTS/Floor/Slums/floor 4/F4runiD1.gd
+++ SCRIPTS/Floor/Slums/floor 4/F4runiD1.gd
@@ -5,9 +5,14 @@ extends Area2D
 
 var dialogue_active = false
 var input_blocked = true  # Prevents input from affecting dialogue at start
+
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
+
+	# Connect to DialogueManager signals
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 	await get_tree().create_timer(0.5).timeout  # Delay input activation
 	input_blocked = false  # Now input can be used, but only inside the area
@@ -23,6 +28,19 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		dialogue_active = false  # Reset when player leaves area
+
+func _on_dialogue_started(_resource: DialogueResource):
+	# Lock player movement when dialogue starts
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.set_movement_locked(true)
+
+func _on_dialogue_ended(_resource: DialogueResource):
+	# Unlock player movement when dialogue ends
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.set_movement_locked(false)
+		dialogue_active = false  # Reset dialogue state
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept"):  # Space by default

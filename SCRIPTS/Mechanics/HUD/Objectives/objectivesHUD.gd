9cc9a65a3f8a038b7ab25c8e43f878f3eb852e35
SKYRENZ <131676044+SKYRENZ@users.noreply.github.com>
2025-03-21 22:12:17 +0800

    objectives hud
    the objectives become green after completion

--- SCRIPTS/Mechanics/HUD/Objectives/objectivesHUD.gd
+++ SCRIPTS/Mechanics/HUD/Objectives/objectivesHUD.gd
@@ -6,23 +6,24 @@ var npc_count: int = 0
 @onready var signage_label = $Panel/Objectives_label/VBoxContainer/SignageObjectives
 @onready var npc_label = $Panel/Objectives_label/VBoxContainer/NPCObjectives
 
+
 func _ready():
-	print("hello world") # Testing purpose
 	ObjectiveManager.connect("objective_updated", Callable(self, "_on_objective_updated"))
 	print("Connected signal: objective_updated")
 
-	# Find the FloorController to access the signage count and NPC count.
+	# Find the FloorController
 	var floor_controller = find_parent("FloorController")
 	if floor_controller:
 		signage_count = floor_controller.signage_count
-		npc_count = floor_controller.npc_count  # Add this line if `npc_count` is exported in FloorController
+		npc_count = floor_controller.npc_count
 		print("ObjectivesHUD: signage_count from FloorController is", signage_count)
 		print("ObjectivesHUD: npc_count from FloorController is", npc_count)
+		
+		# Tell the ObjectiveManager about the totals
+		ObjectiveManager.set_total_objectives(signage_count, npc_count)
 	else:
-		printerr("FloorController not found! Ensure ObjectivesHUD is a child (or grandchild) of the FloorController.")
+		printerr("FloorController not found!")
 		return
-	
-	# Update the HUD initially
 	update_hud_text()
 
 func set_total_objectives_count(signage: int, npcs: int): # CHANGED NAME
@@ -37,20 +38,20 @@ func _on_objective_updated(signage_current: int, signage_total: int, npc_current
 
 func update_hud_text(signage_current: int = 0, signage_total: int = 0, npc_current: int = 0, npc_total: int = 0):
 	if signage_label:
-		if signage_current >= signage_total:
-			signage_label.set("theme_override_colors/default_color", Color("green")) # Make the text green
+		if signage_current >= signage_total and signage_total > 0:
+			signage_label.modulate = Color(0, 1, 0)
 		else:
-			signage_label.set("theme_override_colors/default_color", Color("white")) # set it to default color or whatever you want
-
+			signage_label.modulate = Color(1, 1, 1)
 		signage_label.text = "Signage: %d/%d" % [signage_current, signage_total]
 	else:
 		printerr("Error: Signage Label not found in ObjectivesHUD!")
 
 	if npc_label:
-		if npc_current >= npc_total:
-			npc_label.set("theme_override_colors/default_color", Color("green")) # Make the text green
+		if npc_current >= npc_total and npc_total > 0:
+			npc_label.modulate = Color(0, 1, 0)  # Green
 		else:
-			npc_label.set("theme_override_colors/default_color", Color("white")) # set it to default color or whatever you want
+			npc_label.modulate = Color(1, 1, 1)  # White
+			
 		npc_label.text = "NPC: %d/%d" % [npc_current, npc_total]
 	else:
 		printerr("Error: NPC Label not found in ObjectivesHUD!")

2f3cc0e5e3152583b50806368b16114f8fc69395
Bernardo, John Michael <147980448+itsiKaeel@users.noreply.github.com>
2025-03-21 21:47:51 +0800

    slums 6 to 10
    slums na tapos na po

---
+++ SCENES/FLOOR/Slums/tagilid.gd
@@ -0,0 +1,20 @@
+extends Path2D
+
+@export var speed = 0.5  # Adjust speed as needed
+
+@onready var path_follow = $PathFollow2D
+@onready var animation = $AnimationPlayer
+
+var direction = 1  # 1 = forward, -1 = backward
+
+func _ready():
+	animation.play("move")  # Automatically plays the animation at start
+
+func _process(delta):
+	path_follow.progress_ratio += speed * delta * direction  
+
+	# Reverse direction when reaching the start or end
+	if path_follow.progress_ratio >= 1.0:
+		direction = -1  # Move backward
+	elif path_follow.progress_ratio <= 0.0:
+		direction = 1  # Move forward
